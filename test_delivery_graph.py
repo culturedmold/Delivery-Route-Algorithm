4f8328f9c64ff22d8adc0f2b970fe5a2d5d574bf
Tyler Hampton <122142361+culturedmold@users.noreply.github.com>
2024-01-10 22:24:48 -0700

    Update test_delivery_graph.py

--- test_delivery_graph.py
+++ test_delivery_graph.py
@@ -6,8 +6,8 @@ address_matrix = delivery_graph.Address_Adj_Matrix("csv/addresses.csv", "csv/dis
 
 class Test_Delivery_Graph(unittest.TestCase):
     def test_get_distance_between(self):
-        self.assertIsNone(address_matrix.get_distance_between("hello", "hello"), None)
-        self.assertIsNone(address_matrix.get_address_index("hello"), None)
+        self.assertIsNone(address_matrix.get_distance_between("nil", "nonsense"), None)
+        self.assertIsNone(address_matrix.get_address_index("nothing"), None)
 
 if __name__ == "__main__":
     unittest.main()
